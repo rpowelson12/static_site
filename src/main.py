0bbcf5f6dfe8dcb975e96864d917263179f2de69
rpowelson12 <randy.powelson@yahoo.com>
2025-04-10 12:23:16 -0700

    git pages updates

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
-
+basepath = ""
+if sys.argv[0]:
+    basepath = sys.argv[0]
+else:
+    basepath = "/"
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = dir_path_static + "/content"
 template_path = "./template.html"
 
@@ -20,7 +25,7 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(basepath, dir_path_content, template_path, dir_path_public)
 
 
 main()
