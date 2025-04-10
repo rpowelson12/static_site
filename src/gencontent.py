918b5c48894dbbb93b93db900d6c942cdc2d56a3
rpowelson12 <randy.powelson@yahoo.com>
2025-04-10 12:52:03 -0700

    trying to fix css

--- src/gencontent.py
+++ src/gencontent.py
@@ -30,8 +30,10 @@ def generate_page(from_path, template_path, dest_path, basepath):
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
-    template = template.replace('href="/', 'href="' + basepath)
-    template = template.replace('src="/', 'src="' + basepath)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
+    # You might also need to add this for stylesheets
+    template = template.replace('href="/', f'href="{basepath}')
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
