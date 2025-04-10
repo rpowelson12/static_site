c673f8aa968674ee0f35404b1a6145f76f476cfb
rpowelson12 <randy.powelson@yahoo.com>
2025-04-10 12:47:12 -0700

    url routing changes

--- src/main.py
+++ src/main.py
@@ -5,18 +5,19 @@ import sys
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
-basepath = ""
-if sys.argv[0]:
-    basepath = sys.argv[0]
-else:
-    basepath = "/"
+
 dir_path_static = "./static"
 dir_path_public = "./docs"
-dir_path_content = dir_path_static + "/content"
+dir_path_content = "static/content"
 template_path = "./template.html"
+default_basepath = "/"
 
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -25,7 +26,7 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive(basepath, dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 main()
